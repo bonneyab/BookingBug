9d5f6670c157d792952b5f6c865ab8ac372e88f8
bonneyab <bonneyab@gmail.com>
2015-04-30 11:49:27 -0700

    fix bug in post data

--- Source/BookingBugBookingIntegration/BookingBugOperations/PostData.cs
+++ Source/BookingBugBookingIntegration/BookingBugOperations/PostData.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 using System.Linq;
@@ -14,7 +14,7 @@ namespace BookingBugBookingIntegration.BookingBugOperations
         private readonly ILogger _logger;
 
         public string Name { get { return "postdata"; } }
-        public string Directions { get { return "specify 'postdata' along with the data type (client, bookings) EX: 'postdata bookings' and the postdata.csv file at the JsonFileLocation will be exported to bookingbug"; } }
+        public string Directions { get { return "specify 'bookingbug postdata' along with the data type (client, bookings) EX: 'postdata bookings' and the postdata.csv file at the JsonFileLocation will be exported to bookingbug"; } }
 
         public PostData(IBookingBugApi bookingBugApi, ILogger logger)
         {
@@ -28,9 +28,14 @@ namespace BookingBugBookingIntegration.BookingBugOperations
             var lines = File.ReadAllLines(_path);
             _logger.Info("Sending to booking bug.");
             var headers = lines.First().Split(',');
-            foreach (var dictionary in lines.Skip(1)
-                .Select(lineText => lineText.Split(','))
-                .Select(line => line.ToDictionary(item => headers[Array.IndexOf(line, item)])))
+            var postDataItems = lines.Skip(1)
+                .Select(lineText => lineText.Split(',').Select((value, index) => new {value, index}))
+                .Select(line => line.Aggregate(new Dictionary<string, string>(), (current, next) =>
+                {
+                    current.Add(headers[next.index], next.value);
+                    return current;
+                }));
+            foreach (var dictionary in postDataItems)
             {
                 //This type thing is sort of cheating to pull logic that doesn't really belong here up, it just made things easy and this post data is really a bit of a one-off
                 _bookingBugApi.PostData(args[2], dictionary);
